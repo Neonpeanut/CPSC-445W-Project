5a60a4ce0365e74b3c9289d08068451d676bffbb
Zach <zachary.naumann.14@cnu.edu>
2018-04-16 06:42:03 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+//index.php
+?><!doctype html>
+<head>
+	<title>Library Database</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link href="styles.css" rel="stylesheet">
+</head>
+<body>
+	<header>
+		<h1>Library Database</h1>
+	</header>
+	
+	<main id="center">
+		<h2><a href="books.php">Browse Books</a></h2>
+		<h2><a href="users.php">Manage Users</a></h2>
+	</main>
+	
+	<footer>
+		&copy; 2018 Zachary Naumann
+	</footer>
+</body>
+</html>
